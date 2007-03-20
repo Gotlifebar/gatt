8fa3865431c48f48009300a4688e69606cfcff34
jahlon <jahlon@5941b802-4d28-0410-92f0-476adca567db>
2007-03-20 03:22:35 +0000

    This is just the skeleton of the TTFitnessFunction

---
+++ trunk/gatt/src/optimization/ttga/TTFitnessFunction.java
@@ -0,0 +1,14 @@
+package optimization.ttga;
+
+import org.jgap.FitnessFunction;
+import org.jgap.IChromosome;
+
+public class TTFitnessFunction extends FitnessFunction {
+
+	@Override
+	protected double evaluate(IChromosome individual) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
