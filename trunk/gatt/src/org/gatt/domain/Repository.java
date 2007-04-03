c16b628129dfa17123558c290d6c4a2c46af8416
coolcoder.ex <coolcoder.ex@5941b802-4d28-0410-92f0-476adca567db>
2007-04-03 18:05:26 +0000

    Repository and example class

--- trunk/gatt/src/org/gatt/domain/Repository.java
+++ trunk/gatt/src/org/gatt/domain/Repository.java
@@ -1,35 +1,43 @@
 package org.gatt.domain;
 
-public class Repository {
+
+public abstract class Repository {	
 	
-	public DomainObject searchInMemory(int id, Class type){
-		//1. Search in the repository for the id-type, 
-		//if the object is found return it, else return null.		
-		return null;
-	}
+	public abstract DomainObject searchInMemory(int id, Class type);/*
+		//1. Search in the repository for the id-type,		
+		//if the object is found return it, else return null.
+	*/
 	
 	public DomainObject getObjectById(int id, Class type){
 		//1. CALL searchInMemory, if it's != null then return the object
-		
+		DomainObject dob = searchInMemory(id, type);
+		if( dob != null )
+			return dob;
 		//else
 		
 		//2. CALL addObject(id, type)
-		
+		dob = addObject(id, type);
 		//3. return the added object
-		
-		return null;
+		return dob;		
 	}
-	public void addObject(int id, Class type){
+	
+	protected abstract DomainObject addObject(DomainObject dob);
+	
+	public DomainObject addObject(int id, Class type){
 		//1. Construct a DB Query
 		
 		//2. Execute the query with the DAO
 		
-		//3. Add the object to the repository in memory
-	}
+		//3. Construct the DomainObject from the DAO data (maybe the result set? 
+		//   or an associative array ?)
+		DomainObject dob = null;
+		//4. Add the object to the repository in memory
+		return addObject(dob);
+	}	
 	public int getCardinalityOfType(Class type){
 		//1. Construct a DB Query for count the number of elements of the type
 		
-		//2. Execute the query with the DAO		
+		//2. Execute the query with the DAO
 		
 		return 0;
 	}
--- trunk/gatt/src/org/gatt/domain/Subject.java
+++ trunk/gatt/src/org/gatt/domain/Subject.java
@@ -1,15 +1,12 @@
 package org.gatt.domain;
 
 public class Subject extends DomainObject {
-	private int code;
+	private String code;
 	private String name;
 	private int program;
 	private int semester;
 	
 	public Subject(){
 		
-	}
-	public int getNumericId(){
-		return 0;
-	}
+	}	
 }
