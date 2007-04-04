bb8d1b985b3ed38ca209f0976ae4b70dd54f5c37
coolcoder.ex <coolcoder.ex@5941b802-4d28-0410-92f0-476adca567db>
2007-04-04 21:45:21 +0000

    Makes the transformations of session id and room id into a single int value used for the index inside the chromosome. Also functions to obtain session id and room id from a transformated value.

---
+++ trunk/gatt/src/org/gatt/optimization/util/NumericTransformationFunction.java
@@ -0,0 +1,17 @@
+package org.gatt.optimization.util;
+
+public class NumericTransformationFunction {
+	
+	public int transformIndex(int idSession, int idRoom){
+		// TODO
+		return 0;
+	}
+	public int obtainSessionIdFrom(int value){
+		// TODO
+		return 0;
+	}
+	public int obtainRoomIdFrom(int value){
+		// TODO
+		return 0;
+	}
+}
