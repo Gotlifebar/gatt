a764dc96e578f5fb9bd3951f4feab8f0021612a5
coolcoder.ex <coolcoder.ex@5941b802-4d28-0410-92f0-476adca567db>
2007-04-11 04:10:43 +0000

    Now it stores Integer objects

--- trunk/gatt/src/org/gatt/optimization/util/UniqueRandomNumberGenerator.java
+++ trunk/gatt/src/org/gatt/optimization/util/UniqueRandomNumberGenerator.java
@@ -1,24 +1,24 @@
 package org.gatt.optimization.util;
 
 public class UniqueRandomNumberGenerator {
-	private int[] numbers;
+	private Integer[] numbers;
 	private int uniqueNumbersLeft;
 	/**
 	 * @param totalRandomNumbers  
 	 */
 	public UniqueRandomNumberGenerator(int totalRandomNumbers){		
-		numbers = new int[totalRandomNumbers];
+		numbers = new Integer[totalRandomNumbers];
 		for(int i = 0; i < totalRandomNumbers; i++)
-			numbers[i] = i;
+			numbers[i] = new Integer(i);
 		uniqueNumbersLeft = totalRandomNumbers;
 	}
 	public UniqueRandomNumberGenerator(int lowBound, int highBound){
 		uniqueNumbersLeft = highBound - lowBound;
-		numbers = new int[uniqueNumbersLeft];
+		numbers = new Integer[uniqueNumbersLeft];
 		for(int i = lowBound; i < highBound; i++)
-			numbers[i - lowBound] = i;
+			numbers[i - lowBound] = new Integer(i);
 	}
-	public int nextRandom(){
+	public Integer nextRandom(){
 		//------- This step is for iterative calls ---------
 		//0. If the uniqueNumbersLeft is 0, start again
 		if( uniqueNumbersLeft == 0 )
@@ -28,7 +28,7 @@ public class UniqueRandomNumberGenerator {
 		//2. Decrement the uniqueNumbers left by one
 		uniqueNumbersLeft --;
 		//3. Exchange the number in the random position and the last uniqueNumber left
-		int randomNumber = numbers[position];
+		Integer randomNumber = numbers[position];
 		numbers[position] = numbers[uniqueNumbersLeft];
 		numbers[uniqueNumbersLeft] = randomNumber;
 		//4. Return the randomNumber (the number in the random position)
