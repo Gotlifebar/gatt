d65f478b7d5b3dd61d66016c4862a29ac4c6837d
jahlon <jahlon@5941b802-4d28-0410-92f0-476adca567db>
2007-05-15 02:38:41 +0000

    Shuffler implementation

---
+++ gatt/src/org/gatt/optimization/util/ImpShuffler.java
@@ -0,0 +1,19 @@
+package org.gatt.optimization.util;
+
+public class ImpShuffler implements Shuffler {
+
+	@Override
+	public void shuffle(Object[] arr) {
+		int n = arr.length;
+		int rPos = 0;
+		Object tmp;
+		while( n > 0 ){
+			rPos = (int)(Math.random() * n);
+			n--;
+			tmp = arr[rPos];
+			arr[rPos] = arr[n];
+			arr[n] = tmp;
+		}
+	}
+
+}
