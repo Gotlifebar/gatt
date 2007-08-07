525af8657bbd300bdf3f8862af39fef83c5099c1
jahlon <jahlon@5941b802-4d28-0410-92f0-476adca567db>
2007-08-07 19:41:56 +0000

    Las clases del wizard para definir las restricciones

---
+++ gatt/src/org/gatt/ui/wizards/ConstraintPreviewPanel.java
@@ -0,0 +1,69 @@
+package org.gatt.ui.wizards;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Rectangle;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.TitledBorder;
+
+import org.freixas.jwizard.JWizardPanel;
+
+public class ConstraintPreviewPanel extends JWizardPanel {
+
+	private static final long serialVersionUID = 1L;
+	private JLabel labelConstraint = null;
+	private JPanel pConstraintPreview = null;
+
+	/**
+	 * This is the default constructor
+	 */
+	public ConstraintPreviewPanel() {
+		super();
+		this.setStepTitle("Finalizar creación de restricción");
+		initialize();
+		setBackStep(2);
+	    setNextStep(-1);
+	}
+
+	/**
+	 * This method initializes this
+	 * 
+	 * @return void
+	 */
+	private void initialize() {
+		labelConstraint = new JLabel();
+		labelConstraint.setText("");
+		labelConstraint.setHorizontalTextPosition(SwingConstants.LEFT);
+		labelConstraint.setVerticalAlignment(SwingConstants.TOP);
+		labelConstraint.setVerticalTextPosition(SwingConstants.TOP);
+		labelConstraint.setOpaque(true);
+		labelConstraint.setBackground(Color.white);
+		labelConstraint.setHorizontalAlignment(SwingConstants.LEFT);
+		this.setSize(602, 356);
+		this.getContentPane().setSize(602, 356);
+		this.getContentPane().setLayout(null);
+		this.getContentPane().add(getPConstraintPreview(), null);
+	}
+
+	/**
+	 * This method initializes pConstraintPreview	
+	 * 	
+	 * @return javax.swing.JPanel	
+	 */
+	private JPanel getPConstraintPreview() {
+		if (pConstraintPreview == null) {
+			pConstraintPreview = new JPanel();
+			pConstraintPreview.setLayout(new BorderLayout());
+			pConstraintPreview.setBounds(new Rectangle(15, 15, 572, 323));
+			pConstraintPreview.setBorder(BorderFactory.createTitledBorder(null, "Vista previa de la restricción", TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, new Font("Dialog", Font.BOLD, 12), new Color(51, 51, 51)));
+			pConstraintPreview.add(labelConstraint, BorderLayout.CENTER);
+		}
+		return pConstraintPreview;
+	}
+
+}  //  @jve:decl-index=0:visual-constraint="10,10"
---
+++ gatt/src/org/gatt/ui/wizards/CreateComparationPanel.java
@@ -0,0 +1,282 @@
+package org.gatt.ui.wizards;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Point;
+import java.awt.Rectangle;
+
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JTree;
+import javax.swing.border.TitledBorder;
+
+import org.freixas.jwizard.JWizardPanel;
+
+public class CreateComparationPanel extends JWizardPanel {
+
+	private static final long serialVersionUID = 1L;
+	private JScrollPane scrollTreeLeft = null;
+	private JTree treeLeft = null;
+	private JScrollPane scrollTreeRight = null;
+	private JTree treeRight = null;
+	private JComboBox comboOperator = null;
+	private JRadioButton radioConstant = null;
+	private JRadioButton radioAttribute = null;
+	private JTextField textFreeDomainValue = null;
+	private JPanel pVarLeft = null;
+	private JPanel pVarsRight = null;
+	private JPanel pOperator = null;
+	private JPanel pCompType = null;
+	private JPanel pConstantVal = null;
+	/**
+	 * This is the default constructor
+	 */
+	public CreateComparationPanel() {
+		super();
+		this.setStepTitle("Definir comparación");
+		initialize();
+		setBackStep(0);
+	    setNextStep(2);
+	}
+
+	/**
+	 * This method initializes this
+	 * 
+	 * @return void
+	 */
+	private void initialize() {
+		this.setSize(602, 356);
+		this.getContentPane().setSize(602, 356);
+		this.getContentPane().setLayout(null);
+		this.getContentPane().add(getPVarLeft(), null);
+		this.getContentPane().add(getPVarsRight(), null);
+		this.getContentPane().add(getPOperator(), null);
+		this.getContentPane().add(getPCompType(), null);
+		this.getContentPane().add(getPConstantVal(), null);
+	}
+
+	/**
+	 * This method initializes scrollTreeLeft	
+	 * 	
+	 * @return javax.swing.JScrollPane	
+	 */
+	private JScrollPane getScrollTreeLeft() {
+		if (scrollTreeLeft == null) {
+			scrollTreeLeft = new JScrollPane();
+			scrollTreeLeft.setViewportView(getTreeLeft());
+		}
+		return scrollTreeLeft;
+	}
+
+	/**
+	 * This method initializes treeLeft	
+	 * 	
+	 * @return javax.swing.JTree	
+	 */
+	private JTree getTreeLeft() {
+		if (treeLeft == null) {
+			treeLeft = new JTree();
+		}
+		return treeLeft;
+	}
+
+	/**
+	 * This method initializes scrollTreeRight	
+	 * 	
+	 * @return javax.swing.JScrollPane	
+	 */
+	private JScrollPane getScrollTreeRight() {
+		if (scrollTreeRight == null) {
+			scrollTreeRight = new JScrollPane();
+			scrollTreeRight.setViewportView(getTreeRight());
+		}
+		return scrollTreeRight;
+	}
+
+	/**
+	 * This method initializes treeRight	
+	 * 	
+	 * @return javax.swing.JTree	
+	 */
+	private JTree getTreeRight() {
+		if (treeRight == null) {
+			treeRight = new JTree();
+		}
+		return treeRight;
+	}
+
+	/**
+	 * This method initializes comboOperator	
+	 * 	
+	 * @return javax.swing.JComboBox	
+	 */
+	private JComboBox getComboOperator() {
+		if (comboOperator == null) {
+			comboOperator = new JComboBox();
+			comboOperator.setPreferredSize(new Dimension(120, 25));
+		}
+		return comboOperator;
+	}
+
+	/**
+	 * This method initializes radioConstant	
+	 * 	
+	 * @return javax.swing.JRadioButton	
+	 */
+	private JRadioButton getRadioConstant() {
+		if (radioConstant == null) {
+			radioConstant = new JRadioButton();
+			radioConstant.setText("Valor constante");
+		}
+		return radioConstant;
+	}
+
+	/**
+	 * This method initializes radioAttribute	
+	 * 	
+	 * @return javax.swing.JRadioButton	
+	 */
+	private JRadioButton getRadioAttribute() {
+		if (radioAttribute == null) {
+			radioAttribute = new JRadioButton();
+			radioAttribute.setText("Atributo");
+		}
+		return radioAttribute;
+	}
+
+	/**
+	 * This method initializes textFreeDomainValue	
+	 * 	
+	 * @return javax.swing.JTextField	
+	 */
+	private JTextField getTextFreeDomainValue() {
+		if (textFreeDomainValue == null) {
+			textFreeDomainValue = new JTextField();
+			textFreeDomainValue.setPreferredSize(new Dimension(150, 30));
+		}
+		return textFreeDomainValue;
+	}
+
+	/**
+	 * This method initializes pVarLeft	
+	 * 	
+	 * @return javax.swing.JPanel	
+	 */
+	private JPanel getPVarLeft() {
+		if (pVarLeft == null) {
+			GridBagConstraints gridBagConstraints = new GridBagConstraints();
+			gridBagConstraints.fill = GridBagConstraints.BOTH;
+			gridBagConstraints.weighty = 1.0;
+			gridBagConstraints.weightx = 1.0;
+			pVarLeft = new JPanel();
+			pVarLeft.setLayout(new GridBagLayout());
+			pVarLeft.setBounds(new Rectangle(15, 15, 201, 321));
+			pVarLeft.setFont(new Font("Dialog", Font.PLAIN, 12));
+			pVarLeft.setBorder(BorderFactory.createTitledBorder(null, "Variable lado izquierdo", TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, new Font("Dialog", Font.BOLD, 12), new Color(51, 51, 51)));
+			pVarLeft.add(getScrollTreeLeft(), gridBagConstraints);
+		}
+		return pVarLeft;
+	}
+
+	/**
+	 * This method initializes pVarsRight	
+	 * 	
+	 * @return javax.swing.JPanel	
+	 */
+	private JPanel getPVarsRight() {
+		if (pVarsRight == null) {
+			GridBagConstraints gridBagConstraints1 = new GridBagConstraints();
+			gridBagConstraints1.fill = GridBagConstraints.BOTH;
+			gridBagConstraints1.weighty = 1.0;
+			gridBagConstraints1.weightx = 1.0;
+			pVarsRight = new JPanel();
+			pVarsRight.setLayout(new GridBagLayout());
+			pVarsRight.setLocation(new Point(382, 15));
+			pVarsRight.setSize(new Dimension(201, 222));
+			pVarsRight.setBorder(BorderFactory.createTitledBorder(null, "Atributos", TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, new Font("Dialog", Font.BOLD, 12), new Color(51, 51, 51)));
+			pVarsRight.add(getScrollTreeRight(), gridBagConstraints1);
+		}
+		return pVarsRight;
+	}
+
+	/**
+	 * This method initializes pOperator	
+	 * 	
+	 * @return javax.swing.JPanel	
+	 */
+	private JPanel getPOperator() {
+		if (pOperator == null) {
+			GridBagConstraints gridBagConstraints2 = new GridBagConstraints();
+			gridBagConstraints2.fill = GridBagConstraints.VERTICAL;
+			gridBagConstraints2.gridwidth = 2;
+			gridBagConstraints2.weightx = 1.0;
+			pOperator = new JPanel();
+			pOperator.setLayout(new GridBagLayout());
+			pOperator.setBorder(BorderFactory.createTitledBorder(null, "Operador", TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, new Font("Dialog", Font.BOLD, 12), new Color(51, 51, 51)));
+			pOperator.setSize(new Dimension(144, 52));
+			pOperator.setLocation(new Point(226, 90));
+			pOperator.add(getComboOperator(), gridBagConstraints2);
+		}
+		return pOperator;
+	}
+
+	/**
+	 * This method initializes pCompType	
+	 * 	
+	 * @return javax.swing.JPanel	
+	 */
+	private JPanel getPCompType() {
+		if (pCompType == null) {
+			GridLayout gridLayout1 = new GridLayout();
+			gridLayout1.setRows(2);
+			gridLayout1.setVgap(10);
+			gridLayout1.setColumns(1);
+			GridLayout gridLayout = new GridLayout();
+			gridLayout.setRows(2);
+			gridLayout.setVgap(10);
+			gridLayout.setColumns(1);
+			pCompType = new JPanel();
+			pCompType.setLayout(gridLayout1);
+			pCompType.setBounds(new Rectangle(226, 179, 145, 70));
+			pCompType.setBorder(BorderFactory.createTitledBorder(null, "Tipo de comparación", TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, new Font("Dialog", Font.BOLD, 12), new Color(51, 51, 51)));
+			
+			ButtonGroup group = new ButtonGroup();
+			group.add(getRadioAttribute());
+			group.add(getRadioConstant());
+			
+			pCompType.add(getRadioAttribute(), null);
+			pCompType.add(getRadioConstant(), null);
+		}
+		return pCompType;
+	}
+
+	/**
+	 * This method initializes pConstantVal	
+	 * 	
+	 * @return javax.swing.JPanel	
+	 */
+	private JPanel getPConstantVal() {
+		if (pConstantVal == null) {
+			GridBagConstraints gridBagConstraints3 = new GridBagConstraints();
+			gridBagConstraints3.fill = GridBagConstraints.VERTICAL;
+			gridBagConstraints3.weightx = 1.0;
+			pConstantVal = new JPanel();
+			pConstantVal.setLayout(new GridBagLayout());
+			pConstantVal.setBorder(BorderFactory.createTitledBorder(null, "Valor constante", TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, new Font("Dialog", Font.BOLD, 12), new Color(51, 51, 51)));
+			pConstantVal.setSize(new Dimension(201, 88));
+			pConstantVal.setLocation(new Point(382, 248));
+			pConstantVal.add(getTextFreeDomainValue(), gridBagConstraints3);
+		}
+		return pConstantVal;
+	}
+
+}  //  @jve:decl-index=0:visual-constraint="10,10"
