fa26f3ca2608e1ba212e9cc8b54e2ee603407466
jahlon <jahlon@5941b802-4d28-0410-92f0-476adca567db>
2007-08-08 00:51:23 +0000

    Se agregó el método para escribir una lista de restricciones

--- gatt/src/org/gatt/constraint/io/XMLConstraintWriter.java
+++ gatt/src/org/gatt/constraint/io/XMLConstraintWriter.java
@@ -6,6 +6,9 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import org.gatt.constraint.ConstraintInfo;
 import org.jdom.CDATA;
@@ -82,13 +85,72 @@ public class XMLConstraintWriter {
 		}
 	}
 	
+	public void write(File file, List<ConstraintInfo> constraints){
+		SAXBuilder builder = new SAXBuilder(); 
+		Document doc = null;
+		try{
+			doc = builder.build(file);
+		}catch(IOException ioEx){
+			ioEx.printStackTrace();
+		}
+		catch(JDOMException domEx){
+			domEx.printStackTrace();
+		}
+		
+		ArrayList<Element> cs = new ArrayList<Element>();
+		
+		for (Iterator<ConstraintInfo> iterator = constraints.iterator(); iterator.hasNext();) {
+			ConstraintInfo cInfo = (ConstraintInfo) iterator.next();
+							
+			Element constraint = new Element(CONSTRAINT_ELEMENT);
+			constraint.setAttribute(ID_ATTR,cInfo.getId());
+			constraint.setAttribute(NAME_ATTR,cInfo.getName());
+			constraint.setAttribute(SIGNIFICANCE_ATTR,Double.toString(cInfo.getSignificance()));
+	
+			Element description = new Element(DESCRIPTION_ELEMENT);
+			description.setText(cInfo.getDescription());
+			
+			Element impl = new Element(IMPLEMENTATION_ELEMENT);
+			impl.addContent(new CDATA(cInfo.getStrategyCodeImplementation()));
+			
+			constraint.addContent(description);
+			constraint.addContent(impl);
+			
+			cs.add(constraint);
+		}
+		
+		doc.getRootElement().addContent(cs);
+		
+		FileOutputStream fos;
+		try{
+			fos = new FileOutputStream(file);
+			outp.output(doc, fos);
+		}catch(FileNotFoundException fEx){
+			fEx.printStackTrace();
+		}
+		catch(IOException ioEx){
+			ioEx.printStackTrace();
+		}
+	}
+	
 	public static void main(String ar[]){
 		ConstraintInfo info = new ConstraintInfo();
-		info.setDescription("Mas bla bla bla ...");
-		info.setId("0002");
-		info.setName("Test Constraint two");
+		info.setDescription("Mas bla bla bla ... de la tales 3");
+		info.setId("0003");
+		info.setName("Test Constraint three");
 		info.setSignificance(0.8d);
-		info.setStrategyCodeImplementation("public class Some2{public Some2(){}}");
+		info.setStrategyCodeImplementation("public class Some3{private Some2(){}}");
+		
+		ConstraintInfo info2 = new ConstraintInfo();
+		info2.setDescription("Mas bla bla bla ... y que carajos");
+		info2.setId("0004");
+		info2.setName("Test Constraint four");
+		info2.setSignificance(0.8d);
+		info2.setStrategyCodeImplementation("public class Some4{protected Some4(){}}");
+		
+		ArrayList<ConstraintInfo> list = new ArrayList<ConstraintInfo>();
+		list.add(info);
+		list.add(info2);
 		
 		XMLConstraintWriter writer = XMLConstraintWriter.getInstance();
 		URI uri = null;
@@ -98,7 +160,7 @@ public class XMLConstraintWriter {
 			uriEx.printStackTrace();
 		}
 		File file = new File(uri);
-		writer.write(file,info);
+		writer.write(file,list);
 		
 		System.out.println("File written");
 		
