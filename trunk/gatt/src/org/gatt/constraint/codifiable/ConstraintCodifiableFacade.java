49a0868f88b8fc4179505cfd7bc43940c06a9e21
coolcoder.ex <coolcoder.ex@5941b802-4d28-0410-92f0-476adca567db>
2007-08-12 22:47:02 +0000

    This facade is for the most common operation on this package

---
+++ trunk/gatt/src/org/gatt/constraint/codifiable/ConstraintCodifiableFacade.java
@@ -0,0 +1,29 @@
+package org.gatt.constraint.codifiable;
+
+import org.gatt.constraint.codifiable.boolexpression.BooleanOperand;
+import org.gatt.constraint.codifiable.boolexpression.BooleanOperator;
+import org.gatt.constraint.codifiable.boolexpression.ComparableOperand;
+import org.gatt.constraint.codifiable.boolexpression.CompositeBooleanExpression;
+import org.gatt.constraint.codifiable.boolexpression.DefaultComparison;
+import org.gatt.constraint.codifiable.boolexpression.DefaultComparisonOperator;
+import org.gatt.constraint.codifiable.stringexpression.StringComparableOperand;
+import org.gatt.constraint.codifiable.stringexpression.StringComparison;
+import org.gatt.constraint.codifiable.stringexpression.StringComparisonOperator;
+
+public class ConstraintCodifiableFacade {
+	public ConstraintCodifiableFacade(){
+		
+	}
+	public DefaultComparison createDefaultComparison(DefaultComparisonOperator op, ComparableOperand op1, ComparableOperand op2){
+		return new DefaultComparison(op, op1, op2);
+	}
+	public StringComparison createStringComparison(StringComparisonOperator op, StringComparableOperand op1, StringComparableOperand op2){
+		return new StringComparison(op, op1, op2);
+	}
+	public BooleanOperand addANDComparison(BooleanOperand operand, BooleanOperand comparison){
+		return (new CompositeBooleanExpression(BooleanOperator.AND, operand, comparison));		
+	}
+	public BooleanOperand addORComparison(BooleanOperand operand, BooleanOperand comparison){
+		return (new CompositeBooleanExpression(BooleanOperator.OR, operand, comparison));		
+	}
+}
