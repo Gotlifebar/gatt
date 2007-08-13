c57bef57086c387d9f35116832c859482db752e5
jahlon <jahlon@5941b802-4d28-0410-92f0-476adca567db>
2007-08-13 00:01:26 +0000

    Atributos agregados

--- trunk/gatt/src/org/gatt/domain/Room.java
+++ trunk/gatt/src/org/gatt/domain/Room.java
@@ -1,5 +1,41 @@
 package org.gatt.domain;
 
 public class Room extends DomainObject {
-
+	
+	private String space;
+	private int maxChairs;
+	private int currentChairs;
+	private RoomType type;
+	
+	public Room(){
+		super();
+	}
+	
+	public String getSpace() {
+		return space;
+	}
+	public void setSpace(String space) {
+		this.space = space;
+	}
+	public int getMaxChairs() {
+		return maxChairs;
+	}
+	public void setMaxChairs(int maxChairs) {
+		this.maxChairs = maxChairs;
+	}
+	public int getCurrentChairs() {
+		return currentChairs;
+	}
+	public void setCurrentChairs(int currentChairs) {
+		this.currentChairs = currentChairs;
+	}
+	public RoomType getType() {
+		return type;
+	}
+	public void setType(RoomType type) {
+		this.type = type;
+	}
+	
+	
+	
 }
