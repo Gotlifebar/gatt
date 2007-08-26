270eb60d1ab621dbc007bffecc9cc1123fad3916
coolcoder.ex <coolcoder.ex@5941b802-4d28-0410-92f0-476adca567db>
2007-08-26 15:44:11 +0000

    This is the way the Fitness Function should look like

--- gatt/src/org/gatt/optimization/ttga/TTFitnessFunction.java
+++ gatt/src/org/gatt/optimization/ttga/TTFitnessFunction.java
@@ -1,5 +1,10 @@
 package org.gatt.optimization.ttga;
 
+import java.util.Vector;
+
+import org.gatt.constraint.Constraint;
+import org.gatt.constraint.ConstraintFacade;
+import org.gatt.domain.Session;
 import org.jgap.FitnessFunction;
 import org.jgap.IChromosome;
 
@@ -8,7 +13,19 @@ public class TTFitnessFunction extends FitnessFunction {
 	@Override
 	protected double evaluate(IChromosome individual) {
 		// TODO Auto-generated method stub
-		return 0;
+		Session[] sessions = transform(individual);
+		ConstraintFacade constraintFacade = new ConstraintFacade();
+		Vector<Constraint> constraints = constraintFacade.getCompiledConstraints();
+		double val = 0;
+		for(Constraint c : constraints)
+			val = c.getSignificance() * c.evaluate(sessions).getValue();
+		
+		return 1 / val;
 	}
-
+	
+	private Session[] transform(IChromosome individual){
+		//TODO .... make the transformation.
+		return null;
+	}
+	
 }
