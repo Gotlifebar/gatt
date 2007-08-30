63d81069b3012dce73181baf567ac822017b8042
coolcoder.ex <coolcoder.ex@5941b802-4d28-0410-92f0-476adca567db>
2007-08-30 04:48:14 +0000

    Fixed the bug of the IDs

--- trunk/gatt/src/org/gatt/optimization/ttga/OptimizationFacade.java
+++ trunk/gatt/src/org/gatt/optimization/ttga/OptimizationFacade.java
@@ -239,7 +239,7 @@ public class OptimizationFacade {
 			
 			for (int i = 0; i < genes.length; i++) {
 				genes[i] = new IntegerGene(getConfiguration(),lowBound,numberOfGroups-1);
-				genes[i].setAllele(rand.nextRandom() + 1);
+				genes[i].setAllele(rand.nextRandom());
 			}
 			
 			IChromosome sample = new Chromosome(getConfiguration(),genes);
