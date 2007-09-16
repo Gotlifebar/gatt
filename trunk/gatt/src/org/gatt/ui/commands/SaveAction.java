7ee91c9a937a255d14f391386fc399887543fc19
coolcoder.ex <coolcoder.ex@5941b802-4d28-0410-92f0-476adca567db>
2007-09-16 23:58:47 +0000

    Now it works

--- trunk/gatt/src/org/gatt/ui/commands/SaveAction.java
+++ trunk/gatt/src/org/gatt/ui/commands/SaveAction.java
@@ -3,12 +3,17 @@ package org.gatt.ui.commands;
 import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
-import javax.swing.JOptionPane;
+
+import org.gatt.optimization.io.SolutionIO;
+import org.gatt.optimization.ttga.OptimizationFacade;
 
 public class SaveAction extends AbstractAction {
 
 	public void actionPerformed(ActionEvent e) {
-		JOptionPane.showMessageDialog(null, "Save Action");
+		//JOptionPane.showMessageDialog(null, "Save Action");
+		OptimizationFacade o = OptimizationFacade.getInstance();
+		SolutionIO s = new SolutionIO();
+		s.saveSolution(o.getBestSolution());
 	}
 
 }
